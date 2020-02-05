7438a38ba6120e41ec0f5b3ca76eb00037ca0ee0
Nicholas Kaiser <NicholasKaiser@Nicholass-MacBook-Air.local>
2020-02-04 16:30:54 -0800

    Updated the periodic_callbacks.c and test_periodic_callbacks.c files to accomplish the assigment. switch_led.c and .h files were also added in the l4_io folder.

---
+++ sjtwo-c/projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "unity.h"
+
+// Include the Mocks
+// - This will not pull the REAL source code of these modules (such as board_io.c)
+// - This will auto-generate "Mock" versions based on the header file
+#include "Mockboard_io.h"
+#include "Mockgpio.h"
+
+// Include the source we wish to test
+#include "Mockswitch_led_logic.h"
+#include "periodic_callbacks.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test__periodic_callbacks__initialize(void) {
+  switch_led_logic__initialize_Expect();
+  periodic_callbacks__initialize(); // Why is this line of code here? What is its purpose? Why do we need to call the
+                                    // function we are testing?
+}
+
+void test__periodic_callbacks__1Hz(void) {
+  gpio_s gpio = {};
+  board_io__get_led0_ExpectAndReturn(gpio);
+  gpio__toggle_Expect(gpio);
+
+  switch_led_logic__run_once_Expect();
+  periodic_callbacks__1Hz(
+      0); // Why is this line of code here? What is its purpose? Why do we need to call the function we are testing?
+}
